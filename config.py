6cd30c3e4cd18d58c65e449eb9f660354ce608c1
atunnd <atuan.ngndai@gmail.com>
2024-11-19 00:32:05 +0700

    Hide the uri in db

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+import os
 
-uri = "mongodb+srv://atuanngndai:21Pilots@todolist.f5kei.mongodb.net/?retryWrites=true&w=majority&appName=todolist"
+load_dotenv()
+uri = os.getenv('DB_URI')
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
